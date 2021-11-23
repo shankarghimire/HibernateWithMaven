ae92a300987f6402e6fa35931fd35d045183a034
shankar Ghimire <ssghimire@gmail.com>
2021-11-23 15:47:03 -0500

    Adds the hibernate.cfg.xml configuration file and tested fine

--- src/main/java/com/ghimirsh/HibernateWithMaven/App.java
+++ src/main/java/com/ghimirsh/HibernateWithMaven/App.java
@@ -1,5 +1,7 @@
 package com.ghimirsh.HibernateWithMaven;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 /**
  * Hello world!
  *
@@ -8,6 +10,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Project Started..." );
+        ////Method I: (short cut)
+        SessionFactory  factory = new Configuration().configure().buildSessionFactory();
+        //Method II
+//        Configuration cfg = new Configuration();
+//        cfg.configure("hibernate.cfg.xml");
+//        SessionFactory factory = cfg.buildSessionFactory();
+//        
+        System.out.println(factory);
+        System.out.println(factory.isClosed());
     }
 }
